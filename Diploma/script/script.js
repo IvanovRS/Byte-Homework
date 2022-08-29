8b13d7639202c95f19479f76fe1cc3cfda1984cf
IvanovRS <107371776+IvanovRS@users.noreply.github.com>
2022-08-29 12:56:34 +0300

    Add files via upload

--- Diploma/script/script.js
+++ Diploma/script/script.js
@@ -4,12 +4,6 @@ $('.about-items > div').on('click', function () {
     $(this).next('.about-text').slideToggle(300);
 })
 
-// $('.features_item > p').on('click', function () {
-//     $(this).children('.features_item_title').toggleClass('features_item_title-change-color');
-//     $(this).next('.features_item_text-none').slideToggle(300);
-//     $(this).next('.features_item_hr').toggleClass('features_item_hr-change-color');
-// })
-
 $('.features_item_1 > div').on('click', function () {
     $(this).children('.features_item_title').toggleClass('features_item_title-change-color');
 })
@@ -19,6 +13,6 @@ $('.features_item_1 > div').on('click', function () {
 })    
 
 $('.features_item_1 > div').on('click', function () {
-     $(this).children('.features_item_hr').toggleClass('features_item_hr-change-color');
+     $(this).parent('.features_item_1').toggleClass('features_item_1-change-color');
 }) 
 
